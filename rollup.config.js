6717d079c7c9dfd0e5cf2738df4d5405ac7965ec
leoweyr <contact@leoweyr.com>
2025-12-17 22:07:26 +0800

    build: configure rollup to compile from typescript source files

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,28 @@
-import babel from 'rollup-plugin-babel'
+import typescript from "@rollup/plugin-typescript";
+import babel from 'rollup-plugin-babel';
 
-export default ['cjs', 'es'].map(format => ({
-  input: './src/index.js',
-  output: {
-    file: `./lib/index.${format}.js`,
-    format
-  },
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ],
-  external: ['react', 'react-dom']
-}));
+
+export default ['cjs', 'es'].map(format => (
+    {
+        input: './src/index.ts',
+        output: {
+            file: `./lib/index.${format}.js`,
+            format
+        },
+        plugins: [
+            typescript(
+                {
+                    declaration: true,
+                    declarationDir: './lib',
+                    rootDir: './src'
+                }
+            ),
+            babel(
+                {
+                    exclude: 'node_modules/**'
+                }
+            )
+        ],
+        external: ['react', 'react-dom']
+    }
+));
